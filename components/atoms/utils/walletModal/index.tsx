58657981ad406f4ea3a68d0528d03321610101ad
mateo8392 <shottree657@gmail.com>
2024-05-02 23:23:24 +0900

    fix : style

--- components/atoms/utils/walletModal/index.tsx
+++ components/atoms/utils/walletModal/index.tsx
@@ -34,29 +34,29 @@ export function WalletModal({
         <div className="absolute inset-0 bg-gray opacity-80" />
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4 text-center">
-            <div className="relative w-[100%] max-w-3xl transform overflow-hidden rounded-3xl border-2 border-zinc-800 bg-white text-left shadow-custom dark:border-white-500 dark:bg-gray-700">
-              <div className="flex flex-col items-center justify-between gap-3 border-b-2 border-zinc-800 bg-cover bg-center bg-no-repeat p-6 md:flex-row dark:border-white">
+            <div className="relative w-[100%] max-w-3xl transform overflow-hidden rounded-3xl border-2 border-blue-500 bg-white text-left shadow-custom dark:border-white-500 dark:bg-blue-500">
+              <div className="flex flex-col items-center justify-between gap-3 border-b-2 border-blue-500 bg-cover bg-center bg-no-repeat p-6 md:flex-row dark:border-white">
                 <div className="flex flex-col items-center md:flex-row">
-                  <Image
+                  {/* <Image
                     src="/images/logo.png"
                     alt="Logo"
                     width={32}
                     height={32}
-                  />
+                  /> */}
                   <span
-                    className="pl-2 text-xl font-bold leading-6 dark:text-red-900"
+                    className="pl-2 text-xl text-blue-500 font-bold leading-6 dark:text-white"
                     id="modal-title"
                   >
-                    Please Choose wallet address
+                    Connect your polkadot wallet
                   </span>
                 </div>
 
                 <button
                   type="button"
                   onClick={() => setOpen(false)}
-                  className="rounded-2xl border-4 border-black p-1 transition duration-200 dark:border-white-800 dark:bg-light-dark hover:dark:bg-dark"
+                  className="rounded-2xl border-4 border-blue-500 p-1 transition duration-200 dark:border-white-800 dark:bg-light-dark hover:dark:bg-dark"
                 >
-                  <XMarkIcon className="h-6 w-6 dark:fill-white-blue" />
+                  <XMarkIcon className="h-6 w-6 border-blue-500 dark:fill-white-blue" />
                 </button>
               </div>
               <div className="flex flex-col gap-y-4 overflow-y-auto p-6">
@@ -66,19 +66,19 @@ export function WalletModal({
                     onClick={() => setSelectedAccount(item)}
                     className={`text-md flex cursor-pointer items-center gap-x-3 overflow-auto rounded-xl border-2 p-5 shadow-white dark:text-white ${
                       selectedAccount === item
-                        ? "border-green-500"
+                        ? "border-blue-500 dark:border-white"
                         : "border-black dark:border-white-600 "
                     }`}
                   >
                     <CheckCircleIcon
                       className={`h-6 w-6 ${
                         selectedAccount === item
-                          ? "fill-green-500"
-                          : "fill-black dark:fill-white"
+                          ? "fill-blue-500 dark:fill-white"
+                          : "fill-black dark:white"
                       }`}
                     />
                     <div className="flex flex-col items-start gap-1">
-                      <span className="font-semibold">{item.meta.name}</span>
+                      {/* <span className="font-semibold">{item.meta.name}</span> */}
                       <span>{item.address}</span>
                     </div>
                   </button>
@@ -102,7 +102,7 @@ export function WalletModal({
                 )}
 
                 <button
-                  className="w-44 rounded-xl border-2 border-blue-500 mx-64 text-xl font-semibold text-blue-500 "
+                  className="w-44 rounded-xl border-2 border-blue-500 mx-64 text-xl font-semibold text-blue-500 dark:text-white dark:border-white"
                   onClick={() => {
                     if (!selectedAccount) {
                       toast.error("No address selected", {
